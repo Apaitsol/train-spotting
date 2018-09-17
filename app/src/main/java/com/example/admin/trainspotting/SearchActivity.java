107142b5a23107481ad20381aff4a42b74cd1e56
Kampela <raipalajani@hotmail.com>
2018-09-17 16:23:38 +0300

    Most of UI done, Added fragment for stationlist

--- app/src/main/java/com/example/admin/trainspotting/SearchActivity.java
+++ app/src/main/java/com/example/admin/trainspotting/SearchActivity.java
@@ -1,9 +1,13 @@
 package com.example.admin.trainspotting;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+    FragmentManager manager = getSupportFragmentManager();
+    ItemFragment fragment = ItemFragment.newInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,20 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public void onListFragmentInteraction() {
+
+    }
+
+    public void selectDepartureStation(View view) {
+
+        fragment.show(manager, "Stationlist");
+
+    }
+
+    public void selectDestinationStation(View view) {
+        fragment.show(manager, "Stationlist");
     }
 }
