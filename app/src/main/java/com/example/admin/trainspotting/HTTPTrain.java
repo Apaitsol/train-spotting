4b2494b18a7d3ff57f625331a1fa0f3bffbc8569
Atte <atte.paitsola@gmail.com>
2018-09-17 11:30:15 +0300

    Class for HTTP actions

---
+++ app/src/main/java/com/example/admin/trainspotting/HTTPTrain.java
@@ -0,0 +1,11 @@
+package com.example.admin.trainspotting;
+
+import android.view.View;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class HTTPTrain {
+
+    RequestQueue queue;
+}
